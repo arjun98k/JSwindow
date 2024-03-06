e51a2dd994dffbacfa2d85e53159c1c95bb442a2
arjun98k <arjunkandekar14@gmail.com>
2024-03-06 19:49:20 +0530

    add file

--- STRING.js
+++ STRING.js
@@ -1,7 +1,8 @@
 let a = "arjun kandekar"
 
-console.log(a.length);
-console.log(a.toUpperCase());
-console.log(a.toLowerCase());
-console.log(a.slice(0,5));
-console.log(a.split());
+// console.log(a.length);
+// console.log(a.toUpperCase());
+// console.log(a.toLowerCase());
+// console.log(a.slice(0,5));
+// console.log(a.split());
+console.log(a.indexOf('kandekar'));
